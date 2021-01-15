521baaecd90d2d3245294aecf52c74b8e41eae99
Nizshime Samara <samaramathildes@gmail.com>
2021-01-15 18:22:03 -0300

    alteração Aprendendo commit

--- app/src/test/java/com/nizshime_samara/calculadoraimc/ExampleUnitTest.java
+++ app/src/test/java/com/nizshime_samara/calculadoraimc/ExampleUnitTest.java
@@ -7,8 +7,8 @@ import java.text.DecimalFormat;
 import static org.junit.Assert.*;
 
 /**
- * Example local unit test, which will execute on the development machine (host) comentario.
- *
+ * Example local unit test, which will execute on the development machine (host)
+ * Comentando durante aprendizado
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
